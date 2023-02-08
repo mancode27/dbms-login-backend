87eb4673f61ea62036d9b4247f7a297326dd9d36
Manojkumar Bellatti <manojkumarb.cs20@rvce.edu.in>
2023-02-08 19:30:01 +0530

    First Commit

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const cors = require("cors");
 app.use(cors());
 const bcrypt = require("bcryptjs");
 
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "gnergrigheirg748327bhrbebfr()foeihb!"
+
 const mongoUrl = "mongodb+srv://manoj:manoj@cluster0.owisi19.mongodb.net/?retryWrites=true&w=majority";
 mongoose.set('strictQuery', false);
 mongoose.connect(mongoUrl,{
@@ -39,6 +43,41 @@ app.post("/register", async(req, res) => {
     res.send({status : "error"});
   }
 });
+
+app.post("/login", async (req, res) => {
+  const {email, password} = req.body;
+  const user = await User.findOne({email});
+  if(!user) {
+    return res.json({error : "User not found"});
+  }
+  if(await bcrypt.compare(password, user.password)){
+    const token = jwt.sign({}, JWT_SECRET);
+
+    if(res.status(201)) {
+      return res.json({status : "ok", data : token});
+    }else{ 
+      return res.json({error : "error"});
+    }
+  }
+  res.json({status : "error", error : "Invalid Password"});
+});
+
+app.post("/userData", async (req, res) => {
+  const {token} = req.body;
+  try{
+    const user = jwt.verify(token,JWT_SECRET);
+    const useremail = user.email;
+    User.findOne({email : useremail})
+      .then((data) => {
+        res.send({status : "ok", data : data});
+      })
+      .catch((error) => {
+        res.send({status : "error", data : error});
+      })
+  }catch(err){
+  }
+})
+
 app.listen(3001,() => {
   console.log("server connected!");
 });
